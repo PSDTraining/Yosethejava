d6d4ecc7eb716772c848d3829522a8c6df6dd099
John Doe <john.doe@example.com>
2015-08-13 11:35:52 +0800

    Contact me link

--- src/main/java/yose/Yose.java
+++ src/main/java/yose/Yose.java
@@ -19,11 +19,46 @@ public class Yose {
         final Gson gson = new Gson();
 
         server.start(new DynamicRoutes() {{
-            get("/").to((request, response) -> response.body("Hello Yose"));
+        	
+            get("/").to((request, response) -> response.body(contactMeLink()));
             get("/ping").to(new Ping(gson)::pong);
+            get("/primeFactors?number=16").to(new PowerOfTwo(gson, 16)::prime);
+            get("/aboutme").to((request, response) -> response.body(aboutMe()));
         }});
     }
 
+    public String contactMeLink(){
+    	StringBuilder builder = new StringBuilder();
+    	builder.append("<html>");
+    		builder.append("<a id=\"contact-me-link\" href=\"/aboutme\"/>Contact Information</a>");
+    	builder.append("</html>");
+    	return builder.toString();
+    }
+    
+    public String aboutMe(){
+    	StringBuilder builder = new StringBuilder();
+    	builder.append("<html>");
+    		builder.append("<table>");
+    			builder.append("<tr>");
+	    			builder.append("<td>");
+	    				builder.append("taufiq");
+	    			builder.append("</td>");
+    			builder.append("</tr>");
+    			builder.append("<tr>");
+	    			builder.append("<td>");
+	    				builder.append("ALI");
+	    			builder.append("</td>");
+				builder.append("</tr>");
+				builder.append("<tr>");
+				builder.append("<td>");
+					builder.append("ZAHARY");
+				builder.append("</td>");
+			builder.append("</tr>");
+    		builder.append("</table>");
+    	builder.append("</html>");
+    	return builder.toString();
+    }
+    
     public URI uri() {
         return server.uri();
     }
