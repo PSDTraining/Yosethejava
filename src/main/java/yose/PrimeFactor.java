1a0f03c3be09345849a9df9dc04f7120e5fe71ed
felerminoali <felerminoali@gmail.com>
2015-08-15 14:58:34 +0800

    multiple entries

--- src/main/java/yose/PrimeFactor.java
+++ src/main/java/yose/PrimeFactor.java
@@ -6,7 +6,9 @@ import com.google.gson.Gson;
 import com.vtence.molecule.Request;
 import com.vtence.molecule.Response;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class PrimeFactor {
 
@@ -14,54 +16,78 @@ public class PrimeFactor {
 
 	private int number;
 	private List<Integer> decomposition;
+	
+	private List<List<Integer>> listDecomposition;
 
 	public PrimeFactor(Gson gson) {
 		this.gson = gson;
 	}
 
 	public void prime(Request request, Response response) {
-		// String number = request.parameter("number");
-
-		if (request.parameter("number") != null) {
-			String numString = request.parameter("number");
-			try {
-
-				
-				number = Integer.parseInt(numString);
-				int returnNumber = number;
-				decomposition = new ArrayList<Integer>();
-				
-				if(number> 1000000){
-					throw new IllegalArgumentException();
-				}
-
-				while (number != 1) {
-
-					if (number % 2 == 0) {
-						decomposition.add(2);
-						number = number / 2;
-					} else {
-						int evenNumber = 3;
-						while (evenNumber <= returnNumber) {
-
-							if (number % evenNumber == 0) {
-								decomposition.add(evenNumber);
-								number = number / evenNumber;
-								break;
+	
+		if(request.allParameters()!=null){
+			
+			Map<String, List<String>> paramList = request.allParameters();
+			
+			
+			for (Map.Entry<String, List<String>> entry : paramList.entrySet()) {
+				String key = entry.getKey();
+				if (key.equals("number")) {
+					
+					List<String> values = entry.getValue();
+					listDecomposition = new ArrayList<List<Integer>>();
+					
+					for (String value : values) {
+						
+						try {
+							
+							number = Integer.parseInt(value);
+							int returnNumber = number;
+							decomposition = new ArrayList<Integer>();
+							
+							if(number> 1000000){
+								throw new IllegalArgumentException();
 							}
-							evenNumber += 2;
+
+							while (number != 1) {
+
+								if (number % 2 == 0) {
+									decomposition.add(2);
+									number = number / 2;
+								} else {
+									int evenNumber = 3;
+									while (evenNumber <= returnNumber) {
+
+										if (number % evenNumber == 0) {
+											decomposition.add(evenNumber);
+											number = number / evenNumber;
+											break;
+										}
+										evenNumber += 2;
+									}
+
+								}
+
+							}
+
+							
+							
+						} catch (NumberFormatException e) {
+							response.contentType(JSON).body(gson.toJson(new PrimeError(numString)));
+						} catch (IllegalArgumentException ex){
+							response.contentType(JSON).body(gson.toJson(new PrimeError(numString, "too big number (>1e6)")));
 						}
 
+						listDecomposition.add(decomposition);
 					}
-
 				}
-
-				response.contentType(JSON).body(gson.toJson(new Prime(returnNumber, decomposition)));
-			} catch (NumberFormatException e) {
-				response.contentType(JSON).body(gson.toJson(new PrimeError(numString)));
-			} catch (IllegalArgumentException ex){
-				response.contentType(JSON).body(gson.toJson(new PrimeError(numString, "too big number (>1e6)")));
+				
+				
 			}
+			
+			response.contentType(JSON).body(gson.toJson(new Prime(returnNumber, listDecomposition)));
+			
+
 		}
 
 	}
