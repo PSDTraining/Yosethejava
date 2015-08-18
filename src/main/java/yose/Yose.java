415322f6376711e2f453c04c5572eaf500c53a88
felerminoali <felerminoali@gmail.com>
2015-08-18 19:00:36 +0800

    Prime factor UI AJAX

--- src/main/java/yose/Yose.java
+++ src/main/java/yose/Yose.java
@@ -63,18 +63,26 @@ public class Yose {
 //    	builder.append(" }");
 //    	builder.append("} );");
 //    	builder.append("});");
+    	
+    	builder.append("$('#go').click(function() {");
+    	
+    	
     	builder.append("$(\"#city\").html(\"This is Hello World by JQuery\");");
+    	
+    	builder.append(" });");
+//    	builder.append();
+//    	builder.append();
     	builder.append("});");
     	builder.append("</script>");
 		
 		builder.append("</head>");
-		builder.append("<h1 id=\"title\">Prime Factor - version 6 </h1>");
+		builder.append("<h1 id=\"title\">Prime Factor - version 7 </h1>");
 		builder.append("<br>");
 //		builder.append("<form enctype='multipart/form-data' action='/primeFactors/' method='get'>");
 		builder.append("<form enctype='multipart/form-data' >");
 		builder.append("<p id=\"invitation\">Insert a number</p>");
 		builder.append("<input  type=\"text\" id=\"number\" name=\"number\">");
-		builder.append(" <button type=\"submit\" id=\"go\">Go</button> ");
+		builder.append(" <button id=\"go\">Go</button> ");
 		builder.append("<p id=\"city\"></p>");
 		builder.append("</form>");
 		 builder.append("</html>");
