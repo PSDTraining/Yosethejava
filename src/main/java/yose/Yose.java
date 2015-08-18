d01a27c67db15c2dfcd1080de6cef294ec4f4365
felerminoali <felerminoali@gmail.com>
2015-08-18 20:44:29 +0800

    Prime factor UI AJAX

--- src/main/java/yose/Yose.java
+++ src/main/java/yose/Yose.java
@@ -80,7 +80,7 @@ public class Yose {
     	builder.append("dataType: 'json',");
     	builder.append("success: function (data) {");
 //    	builder.append("var obj = JSON.parse(data);");
-    	builder.append("alert(data.responseTex);");
+    	builder.append("alert(JSON.parse(data));");
 //    	builder.append("$('#city').html(data.responseTex);");
     	builder.append("}");
     	builder.append(",");
@@ -101,7 +101,7 @@ public class Yose {
     	builder.append("</script>");
 		
 		builder.append("</head>");
-		builder.append("<h1 id=\"title\">Prime Factor - version 23 </h1>");
+		builder.append("<h1 id=\"title\">Prime Factor - version 24 </h1>");
 		builder.append("<br>");
 //		builder.append("<form enctype='multipart/form-data' action='/primeFactors/' method='get'>");
 		builder.append("<form enctype='multipart/form-data' >");
