9f3e4feef1f4cf8df81e62c0941553920f969504
felerminoali <felerminoali@gmail.com>
2015-08-22 23:15:40 +0800

    PrimeFactor jquery GET

--- src/main/java/yose/Yose.java
+++ src/main/java/yose/Yose.java
@@ -54,11 +54,11 @@ public class Yose {
     	builder.append("var results = document.getElementById(\"results\");");
     	builder.append("var hr = new XMLHttpRequest();");
     	builder.append("hr.open(\"GET\", \"/primeFactors?number=20\", true);");
-//    	builder.append("hr.setRequestHeader(\"Content-type\", \"application/json\", true);");
-//    	builder.append(" hr.onreadystatechange = function() { ");
-//    	builder.append("if(hr.readyState == 4 && hr.status == 200) { ");
-////    	builder.append("var data = JSON.parse(hr.responseText); ");
-//    	builder.append("results.innerHTML = \"olaaaaaaaaaaa\"; ");
+    	builder.append("hr.setRequestHeader(\"Content-type\", \"application/json\", true);");
+    	builder.append(" hr.onreadystatechange = function() { ");
+    	builder.append("if(hr.readyState == 4 && hr.status == 200) { ");
+//    	builder.append("var data = JSON.parse(hr.responseText); ");
+    	builder.append("results.innerHTML = \"olaaaaaaaaaaa\"; ");
 //    	builder.append(" }");
 //    	builder.append(" }");
     	builder.append("hr.send(); ");
