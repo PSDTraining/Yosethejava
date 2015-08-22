0aa819c8a331f09c1ac5216fd390c0c17c98d6cd
felerminoali <felerminoali@gmail.com>
2015-08-22 22:08:42 +0800

    PrimeFactor jquery GET

--- src/main/java/yose/Yose.java
+++ src/main/java/yose/Yose.java
@@ -55,7 +55,7 @@ public class Yose {
     	builder.append("function ajax_get_json(){ ");
     	builder.append("var results = document.getElementById(\"results\");");
     	builder.append("var hr = new XMLHttpRequest();");
-    	builder.append(" hr.open(\"GET\", \"/primeFactors/?number=20\", true);");
+    	builder.append(" hr.open(\"GET\", \"yosejava.herokuapp.com/primeFactors/?number=20\", true);");
     	builder.append("hr.setRequestHeader(\"Content-type\", \"application/json\", true);");
     	builder.append(" hr.onreadystatechange = function() { ");
     	builder.append("if(hr.readyState == 4 && hr.status == 200) { ");
@@ -71,7 +71,7 @@ public class Yose {
     	builder.append("</script>");
 		
 		builder.append("</head>");
-		builder.append("<h1 id=\"title\">Prime Factor - version 31 </h1>");
+		builder.append("<h1 id=\"title\">Prime Factor - version 32 </h1>");
 		builder.append("<br>");
 		builder.append("<form enctype='multipart/form-data' >");
 		builder.append("<p id=\"invitation\">Insert a number</p>");
