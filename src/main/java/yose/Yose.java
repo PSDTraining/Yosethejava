0a1ed4c298b1e7fb4d54e541eff7321978845555
felerminoali <felerminoali@gmail.com>
2015-08-22 23:04:00 +0800

    PrimeFactor jquery GET

--- src/main/java/yose/Yose.java
+++ src/main/java/yose/Yose.java
@@ -69,7 +69,7 @@ public class Yose {
     	builder.append("</script>");
 		
 		builder.append("</head>");
-		builder.append("<h1 id=\"title\">Prime Factor - version 39 </h1>");
+		builder.append("<h1 id=\"title\">Prime Factor - version 40 </h1>");
 		builder.append("<br>");
 		builder.append("<form enctype='multipart/form-data' >");
 		builder.append("<p id=\"invitation\">Insert a number</p>");
