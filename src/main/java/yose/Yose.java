07abb4073c34f5c812a661acd081a13087da7580
felerminoali <felerminoali@gmail.com>
2015-08-22 22:45:58 +0800

    PrimeFactor jquery GET

--- src/main/java/yose/Yose.java
+++ src/main/java/yose/Yose.java
@@ -53,11 +53,11 @@ public class Yose {
     	builder.append("function ajax_get_json(){ ");
     	builder.append("var results = document.getElementById(\"results\");");
     	builder.append("var hr = new XMLHttpRequest();");
-    	builder.append(" hr.open(\"GET\", \"/primeFactors?number=20\", true);");
+    	builder.append(" hr.open(\"GET\", \"primeFactors?number=20\", true);");
     	builder.append("hr.setRequestHeader(\"Content-type\", \"application/json\", true);");
     	builder.append(" hr.onreadystatechange = function() { ");
     	builder.append("if(hr.readyState == 4 && hr.status == 200) { ");
-    	builder.append("var data = JSON.parse(hr.responseText); ");
+//    	builder.append("var data = JSON.parse(hr.responseText); ");
     	builder.append("results.innerHTML = \"olaaaaaaaaaaa\"; ");
     	builder.append(" }");
     	builder.append(" }");
@@ -69,7 +69,7 @@ public class Yose {
     	builder.append("</script>");
 		
 		builder.append("</head>");
-		builder.append("<h1 id=\"title\">Prime Factor - version 36 </h1>");
+		builder.append("<h1 id=\"title\">Prime Factor - version 37 </h1>");
 		builder.append("<br>");
 		builder.append("<form enctype='multipart/form-data' >");
 		builder.append("<p id=\"invitation\">Insert a number</p>");
